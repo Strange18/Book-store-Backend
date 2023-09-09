583a183f5366787f6e3d369c88d8171ba12baa10
Strange18 <prashant.bista.18@gmail.com>
2023-09-09 23:19:10 +0545

    database cconnection established

---
+++ app.js
@@ -0,0 +1,20 @@
+// for loading environment variable
+require("dotenv").config();
+
+//  for server
+const express = require("express");
+const app = express();
+const PORT = 5000;
+
+// connecting to database
+const connectDB = require("./server/database/config/db");
+connectDB();
+
+// router configurations
+const main_router = require("./server/routes/main_route");
+app.use("/", main_router);
+
+// running the server
+app.listen(PORT, () => {
+  console.log(`Starting servvice at port ${PORT}`);
+});
